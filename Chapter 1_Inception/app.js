e735589e3c2bc939db5cf4d4e73fcce274215526
Akshat <akshat.02jan@gmail.com>
2023-10-28 00:08:10 +0530

    First react code

---
+++ Chapter 1_Inception/app.js
@@ -0,0 +1,5 @@
+const parent = React.createElement("div",{id:"heading"},[React.createElement("h1",{id:"content"},"Hello I am Akshat"),React.createElement("h2",{id:"content"},"Hello I am Akshat2 ")]);
+
+        const root = ReactDOM.createRoot(document.getElementById("root"));
+
+        root.render(parent);
